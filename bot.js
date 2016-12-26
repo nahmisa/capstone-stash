ba84867bbd3c06c7f344696bbd76c56f8bfe8d3e
Sarah Nahmias <nahmisa@gmail.com>
2016-12-26 14:26:34 -0800

    Refactors with variable name

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 var botBuilder = require('claudia-bot-builder'),
     excuse = require('huh');
 
-module.exports = botBuilder(function (request) {
+var stashBot = botBuilder(function (request) {
   return 'Thanks for sending ' + request.text  +
       '. Your message is very important to us, but ' +
       excuse.get();
 });
+
+module.exports = stashBot;
