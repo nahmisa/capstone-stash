93cc6709c8c67ffa5eb17ff450cd5d1b7efcd4b0
Sarah Nahmias <nahmisa@gmail.com>
2017-01-05 20:49:49 -0800

    Gets JSON from LUIS API and is able to put it in the database

--- bot.js
+++ bot.js
@@ -3,70 +3,77 @@ var AWS = require("aws-sdk");
 AWS.config.update({
   region: "us-west-2",
   endpoint: "https://dynamodb.us-west-2.amazonaws.com"
-    // endpoint: "http://localhost:8000"
+  // endpoint: "http://localhost:8000"
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
+
+var request = require('request-promise');
+
 var botBuilder = require('claudia-bot-builder');
 
-var stashBot = botBuilder(function(request) {
+var stashBot = botBuilder(function(user_input) {
 
-
-  var current_type = request.text;
+  var current_type = 'test';
 
   var table = "Milk";
 
   var date = Date.now();
 
-  var params = {
-      TableName: table,
-      Item:{
+  var options = {
+    uri: 'https://api.projectoxford.ai/luis/v2.0/apps/c2c0c678-0ed6-41de-8abc-436a367927a9?subscription-key=18c637e3c39947c789395c46453683e7&verbose=true&',
+    qs: {
+      q: 'pumped' // -> uri + '?q=xxxxx%20xxxxx'
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  return request(options)
+    .then(function (data) {
+
+      console.log(data);
+      var params = {
+        TableName: table,
+        Item:{
           "date": date,
           "type": current_type,
           "info":{
-              "something": "Foo",
-              "something2": 0
+            "intent": data.topScoringIntent.intent,
+            "entity": JSON.stringify(data.entities)
           }
-      }
-  };
+        }
+      };
 
-  // // for get
-  var params2 = {
-      TableName: table,
-      Key:{
+      return docClient
+        .put(params)
+        .promise();
+    })
+    .then(function(data) {
+      console.log('>>>> ADD DATA' + JSON.stringify(data));
+
+      var params2 = {
+        TableName: table,
+        Key:{
           "date": date,
           "type": current_type
-      }
-  };
+        }
+      };
 
-  var addPromise = docClient.put(params).promise();
-
-  addPromise.then(function(data) {
-    // return "Added item:" + JSON.stringify(data, null, 2);
-  }).catch(function(err) {
-    // return "Unable to add item. Error JSON:" + JSON.stringify(err, null, 2);
-  });
-
-
-  var getPromise = docClient.get(params2).promise();
-
-  getPromise.then(function(data) {
-    // return 'Thanks for sending ' + type  +
-    //     '.  Returns >>> ' + data;
-  }).catch(function(err) {
-    // return 'Thanks for sending ' + type  +
-    //     '.  Something went wrong';
-  });
-
-  var response = Promise.all([addPromise, getPromise]).then(function(values) {
-    console.log(values);
-    return JSON.stringify(values[1].Item) + " >>>> " + request.text;
-
-  });
-
-  return response;
+      return docClient
+        .get(params2)
+        .promise();
+    })
+    .then(function(data) {
+      console.log('>>>> GET data' + JSON.stringify(data));
+      return 'I made it here!';
+    })
+    .catch(function(err) {
+      return 'Sorry something went wrong :(';
+    });
 
 });
-
 
 module.exports = stashBot;
