23ee1d093e5945d9f5de16bd2177a40e640b6fd7
Sarah Nahmias <nahmisa@gmail.com>
2017-01-20 12:03:17 -0800

    Removes spacing issues

--- bot.js
+++ bot.js
@@ -21,36 +21,30 @@ var stashBot = botBuilder(function(user_input, originalApiRequest) {
   var nlpURL = 'https://api.projectoxford.ai/luis/v2.0/apps/c2c0c678-0ed6-41de-8abc-436a367927a9?subscription-key=18c637e3c39947c789395c46453683e7&q=';
 
   return fetch(nlpURL + user_input.text)
-    // extract JSON of user intent and entities from LUIS api
     .then(function(result){
-
+    // extract JSON of user intent and entities from LUIS api
       return result.json();
     })
-    // Get the intent from the API data and use to determine the action/method
     .then(function(data) {
-
+    // Get the intent from the API data and use to determine the action/method
       action = data.topScoringIntent.intent;
-
+      // Error-handle the case that the user's intent is not understood
       if (action == "None") throw "I didn't undersand what you said";
 
       return methods[action](data.entities);
     })
-    // Calculate total amounts of milk (after action) to use in response
     .then(function(data) {
-
+    // Calculate total amounts of milk (after action) to use in response
       return response.calculateAmount();
     })
-    // Display appropriate reponse to user based on their inital action-intent
     .then(function(totals) {
-
+    // Display appropriate reponse to user based on their inital action-intent
       return response.createOutput(action, user_input.text, totals);
     })
-    // Appologize for any errors and give user-friendly details
     .catch(function(err) {
-
+    // Appologize for any errors and give user-friendly details
       return 'Sorry something went wrong :(. ' + err;
     });
-
 });
 
 module.exports = stashBot;
