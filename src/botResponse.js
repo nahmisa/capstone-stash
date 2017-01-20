c7d6f9258614a9ff24b5dd38cd97d04839bc9c48
Sarah Nahmias <nahmisa@gmail.com>
2017-01-20 12:37:38 -0800

    Updates botResponse with comments

--- src/botResponse.js
+++ src/botResponse.js
@@ -2,104 +2,70 @@ var dictionary = require('./dictionary');
 var methods = dictionary.init();
 
 var calculateAmount = async function() {
-
   var milkTotals = {};
-
+  // Available total (does not include consumed)
   milkTotals.total = await methods['Display']([]);
-
+  // Fresh
   milkTotals.fresh = await methods['Display']([{
       "entity": "fresh",
       "type": "Type"
       }]);
-
+  // Frozen
   milkTotals.frozen = await methods['Display']([{
       "entity": "frozen",
       "type": "Type"
       }]);
-
+  // Thawed
   milkTotals.thawed = await methods['Display']([{
       "entity": "thawed",
       "type": "Type"
       }]);
-
+  // Consumed
   milkTotals.consumed = await methods['Display']([{
       "entity": "consumed",
       "type": "Type"
       }]);
 
   return milkTotals;
-
 };
 
-// can use milk totals to give an overview of the state of the db.
 var createOutput = function(action, query, totals) {
-  // based on the aciton, we will provide different output
-  console.log("This should be a dictionary of totals", totals);
+  // Based on the action, provide different output
   switch (action) {
   case 'Display':
-    console.log('You said: ' + query + ". " +
-      "There are " + totals.total + " ounces of milk total, consisting of: ." +
-      totals.fresh + " fresh ounces, " +
-      totals.frozen + " frozen ounces, and " +
-      totals.thawed + " thawed ounces, ");
-
-    return 'You said: ' + query + ". " +
+    return "You said: " + "'" +query + "'. " +
       "There are " + totals.total + " ounces of milk total, consisting of: ." +
       totals.fresh + " fresh ounces, " +
       totals.frozen + " frozen ounces, and " +
       totals.thawed + " thawed ounces.";
-
   case 'Add':
-  // report all the milk that has not been consumed when a mom adds milk
-    console.log('You said: ' + query + ". " +
-      "After adding that milk, there are " + totals.total + " ounces of consumable milk total." +
-      "Since you've been tracking, you've added " + (totals.consumed + totals.total) + " ounces - way to go!");
-
-    return 'You said: ' + query + ". " +
+  // Report all the milk - including the consumed milk
+    return "You said: " + "'" +query + "'. " +
       "After adding that milk, there are " + totals.total + " ounces of consumable milk total." +
       "Since you've been tracking, you've added " + (totals.consumed + totals.total) + " ounces - way to go!";
-
-
   case 'Use':
-    // report all that has not been consumed.
-    console.log('You said: ' + query + " " +
-      "After using that milk, there are " + totals.total + " ounces of consumable milk left, consisting of: " +
-      totals.fresh + " fresh ounces, " +
-      totals.frozen + " frozen ounces, and " +
-      totals.thawed + " thawed ounces. " +
-      "Since you've been tracking, Baby has consumed " + totals.consumed + " ounces - woah!");
-
-    return 'You said: ' + query + " " +
+    // Report all that has not been consumed.
+    // Give detail of baby's consumption later
+    return "You said: " + "'" +query + "'. " +
       "After using that milk, there are " + totals.total + " ounces of consumable milk left, consisting of: " +
       totals.fresh + " fresh ounces, " +
       totals.frozen + " frozen ounces, and " +
       totals.thawed + " thawed ounces. " +
       "Since you've been tracking, Baby has consumed " + totals.consumed + " ounces - woah!";
-
   case 'Thaw':
-
-    console.log('You said: ' + query + " " +
-      "After thawing that milk there are " + totals.thawed + " ounces of thawed milk.  Remeber, this milk expires in 24 hours so use it up! There are still " +
-      totals.frozen + " frozen ounces.");
-
-    return 'You said: ' + query + " " +
+    // Report thawed and still frozen
+    return "You said: " + "'" +query + "'. " +
       "After thawing that milk there are " + totals.thawed + " ounces of thawed milk.  Remeber, this milk expires in 24 hours so use it up! There are still " +
       totals.frozen + " frozen ounces.";
 
   case 'Freeze':
-
-    console.log('You said: ' + query + " " +
-      "After freezing that milk there are " + totals.frozen + " ounces of thawed milk. There are still " +
-      totals.fresh + " fresh ounces.");
-
-    return 'You said: ' + query + " " +
+    // Report frozen and still fresh
+    return "You said: " + "'" +query + "'. " +
       "After freezing that milk there are " + totals.frozen + " ounces of thawed milk. There are still " +
       totals.fresh + " fresh ounces.";
-
   default:
     return 'Great I got it! ' + totals.total;
   }
-
 };
 
 module.exports = {
